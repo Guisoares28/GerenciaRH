a280d384fb3a7cb8583b6a8ff0f4bd6e7c3925b2
gui.2899 <gui.2899>
2025-05-28 13:09:22 -0300

    ajustando DTO

--- src/main/java/com/gerenciarh/gerenciarh/DtosResponse/TokenResponseDTO.java
+++ src/main/java/com/gerenciarh/gerenciarh/DtosResponse/TokenResponseDTO.java
@@ -1,7 +1,32 @@
 package com.gerenciarh.gerenciarh.DtosResponse;
 
-public record TokenResponseDTO(
-        String token,
-        boolean status
-) {
+public class TokenResponseDTO{
+
+    private String token;
+
+    private boolean status;
+
+    public TokenResponseDTO(String token, boolean status) {
+        this.token = token;
+        this.status = status;
+    }
+
+    public TokenResponseDTO() {
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
